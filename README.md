1e0305bef55ffe41dba69cc47034d391ae7232ed
Deli Laszlo <deli.laszlo.viktor@hallgato.sze.hu>
2025-10-26 15:42:36 +0100

    Readded login page and updated readme

--- README.md
+++ README.md
@@ -5,19 +5,18 @@ Hallgatói fájlmegosztó projektmunka, SZE 2025/26-1
     <li><a href="https://www.apachefriends.org/">XAMPP</a> letöltés</li>
     <li>XAMPP Control Panel -> Start Apache, MySQL</li>
     <li>Repo klónozása a C:\xampp\htdocs mappába</li>
+    <li>Böngészőn belül megnyitni a localhost/phpmyadmin oldalt</li>
+    <li>Databases fülön Create Database pm_db_fm_v1 névvel</li>
+    <li>Import fülön adatbazis.sql importálása</li>
     <li>Böngészőn belül megnyitni a localhost/hallgatoi-fajlmegoszto/ kezdőoldalt</li>
 </ul>
 <ul>
     <li>A phpmyadmin kezelése:</li>
-    <li>XAMPP-ban indítsd el a MySQL és Apache szolgáltatásokat.</li>
-    <li>A kezelőfelület megnyitása: <b>localhost/phpmyadmin ezt elég a search bar-ba beírni</b>.</li>
-    <li>Importáld az <b>adatbazis.sql</b> fájlt.</li>
     <li>A megfelelő adatbázis lenyitása után a táblákra kattintva tudod azokat módosítani.</li>
     <li>A <b>Szerkezet</b> fülön tudod módosítani a tábla felépítését.</li>
     <li>A <b>Tartalom</b> fülön megtekintheted az adatok listáját.</li>
     <li>Új adatot feltölteni vagy lekérdezést futtatni az <b>SQL</b> fülön tudsz.</li>
     <li>Az adatbázishoz való csatlakozás módja részletesen, megjegyzésekkel együtt megtalálható a <b>login.php</b> fájlban.</li>
-    <li>FONTOS: a <b>login.php</b> és <b>registration.php</b> az <b>index.php</b>-ra <b>log_reg</b>-ként hivatkozik, használat előtt írd át ennek megfelelően.</li>
     <li>Az adatbázisban már található egy teszt felhasználó:<br>
         <b>Neptun:</b> asd123<br>
         <b>Jelszó:</b> 12345678
---
+++ log_reg.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/x-icon" href="https://munkatars.sze.hu/core/templates/sze2018_bluerev/favicon.ico?v=2">
+    <title>Hallgatói fájlmegosztó</title>
+</head>
+<body>
+    <img src="icons/IVK_logo.png" alt="IVK logo" class="ivk_logo" id="login_ivk_logo" width="1920" height="586">
+    <div id="index_container">
+        <div id="index_header">
+            <h1>Hallgatói Fájlmegosztó</h1>
+            <p>Széchenyi István Egyetem</p>
+        </div>
+        <div id="login" >
+            <h1>Bejelentkezés</h1>
+            <form id="loginForm" action="login.php" method="post">
+                <input type="text" id="loginNeptun" name="neptun" placeholder="Neptun kód" required autofocus>
+                <input type="password" id="loginPassword" name="password" placeholder="Jelszó" required>
+                <button type="submit">Bejelentkezés</button>
+                <a href="#" id="showRegister">Regisztráció</a>
+            </form>
+        </div>
+        <div id="register" style="display: none;">
+            <h1>Regisztráció</h1>
+            <form id="registerForm" action="registration.php" method="post"> 
+                <input type="text" id="registerNeptun" name="neptun" placeholder="Neptun kód" required autofocus>
+                <input type="text" id="registerUsername" name="username" placeholder="Felhasználónév" required>
+                <input type="text" id="registerFullname" name="fullname" placeholder="Teljes név" required>
+                <input type="email" id="registerEmail" name="email" placeholder="Email" required>
+                <input type="password" id="registerPassword" name="password" placeholder="Jelszó" required>
+                <input type="password" id="registerConfirmPassword" name="confirm_password" placeholder="Jelszó újra" required>
+                <button type="submit">Regisztráció</button>
+                <a href="#" id="showLogin">Bejelentkezés</a>
+            </form>
+        </div>
+    </div>
+    <script type="text/javascript" src="scripts.js"></script>
+</body>
+</html>
